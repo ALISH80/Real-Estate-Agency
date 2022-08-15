ae5ace6f80358d3a92fcf3e7f439a894ef7706fc
atoofihub <atoofi_alireza@yahoo.com>
2022-08-15 21:43:46 +0430

    changed without move bag

--- js/js.js
+++ js/js.js
@@ -1,16 +1,20 @@
 console.log('ok');
 
-let right_state = true
+let right_state = true;
+let last_clicked_id;
 $('.btn_1').click(function (e) {
     let w = $(this).width();
-    let myElement = document.querySelector('.btn-back');
-    if(right_state){
-        myElement.style.transform = "translateX(" + (-w*1.3) + "px)";
-        right_state = false;
-    }
-    else {
-        myElement.style.transform = "translateX(" + (0) + "px)";
-        right_state = true;
+    if (last_clicked_id !== $(this).attr('id')){
+        let myElement = document.querySelector('.btn-back');
+        if(right_state){
+            myElement.style.transform = "translateX(" + (-w*1.3) + "px)";
+            right_state = false;
+        }
+        else {
+            myElement.style.transform = "translateX(" + (0) + "px)";
+            right_state = true;
+        }
+        last_clicked_id = $(this).attr('id');
     }
 
 });
